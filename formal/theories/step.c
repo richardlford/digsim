16d0a3d2b2c990bf49ebb0a7637b2724e9f20e95
Richard L Ford <richardlford@gmail.com>
2019-03-07 17:01:27 -0600

    ckp

--- formal/theories/step.c
+++
@@ -1,9 +0,0 @@
-// A step in a Euler solver
-#include "state.h"
-
-void step(State* stp, State (*xddp)(State*), double dt) {
-  State derivatives = xddp(stp);
-  for(int i=0; i < StateSize; i++) {
-    stp->StateVector[i] += derivatives.StateVector[i] * dt;
-  }
-}
--- formal/theories/stepn.c
+++
@@ -1,6 +0,0 @@
-// A step in a Euler solver
-void stepn(double* state, double* diffs, unsigned n, double dt) {
-  for (int i = 0; i < n; i++) {
-    state[i] += diffs[i] * dt;
-  }
-}
